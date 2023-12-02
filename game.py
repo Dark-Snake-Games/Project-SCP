7b7c4f4183cb47ea47475c72273d3faabbe52891
R9_ <neamtrares@naver.com>
2023-12-02 21:56:35 +0200

    Separated SCPs and game into 2 files

--- game.py
+++ game.py
@@ -3,34 +3,12 @@ from pygame import Vector2
 from pygame.display import update
 from sys import exit
 from random import randint
+from scps import SCP999
 
 from DSEngine.animated import AnimationSheet
 default_title="Project: SCP"
 click_sound = AudioPlayer("beep.mp3")
 AUDIO_MAN = AudioManager(click=click_sound)
-
-class SCP999(AnimatedSprite2D):
-    def __init__(self, position=...):
-        SCP999_1 = Image2D("Test.png")
-        SCP999_2 = Image2D("Test1.png")
-        walk_sheet = Spritesheet(0, SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,\
-                                SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,)
-        sheet = AnimationSheet(default=Image2D("Test.png"), walk=walk_sheet)
-        self.last_secs = 0
-        super().__init__(sheet, 1, position)
-    
-    def render(self, window: Window):
-        if self.moving_towards and not self.playing:
-            self.play_sheet("walk")
-        if not self.moving_towards and self.playing:
-            self.stop_playing()
-        if int(window.seconds-self.last_secs) >= 25:
-            rx = randint(0, window.size[0])
-            ry = randint(0, window.size[1])
-            pos = Vector2(rx, ry)
-            self.move_towards(pos)
-            self.last_secs = int(window.seconds)
-        super().render(window)
 
 def main_menu():
     global default_title, AUDIO_MAN
---
+++ scps.py
@@ -0,0 +1,26 @@
+from DSEngine import *
+from pygame import Vector2
+from random import randint
+
+class SCP999(AnimatedSprite2D):
+    def __init__(self, position=...):
+        SCP999_1 = Image2D("Test.png")
+        SCP999_2 = Image2D("Test1.png")
+        walk_sheet = Spritesheet(0, SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,SCP999_1,\
+                                SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,SCP999_2,)
+        sheet = AnimationSheet(default=Image2D("Test.png"), walk=walk_sheet)
+        self.last_secs = 0
+        super().__init__(sheet, 1, position)
+    
+    def render(self, window: Window):
+        if self.moving_towards and not self.playing:
+            self.play_sheet("walk")
+        if not self.moving_towards and self.playing:
+            self.stop_playing()
+        if int(window.seconds-self.last_secs) >= 25:
+            rx = randint(0, window.size[0])
+            ry = randint(0, window.size[1])
+            pos = Vector2(rx, ry)
+            self.move_towards(pos)
+            self.last_secs = int(window.seconds)
+        super().render(window)
